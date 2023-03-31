1bbd421c07ef3cf81c8b566e98e7d6099e24b4b3
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-03-31 13:53:59 -0300

    todas as sections prontas

--- dist/scripts/js/main.js
+++ dist/scripts/js/main.js
@@ -1,1 +1,25 @@
-const navMobile=document.querySelector("#nav-mobile"),iconMenu=document.querySelector("#iconMenu");let clickMenu=!1;function menu(){clickMenu=0==clickMenu?(navMobile.classList.remove("header__nav__mobile--is--closed"),navMobile.classList.add("header__nav__mobile--is--open"),iconMenu.setAttribute("src","./dist/shared/image/icon/menux.svg"),!0):(navMobile.classList.remove("header__nav__mobile--is--open"),navMobile.classList.add("header__nav__mobile--is--closed"),iconMenu.setAttribute("src","./dist/shared/image/icon/menu.svg"),!1)}
+const navMobile=document.querySelector("#nav-mobile"),iconMenu=document.querySelector("#iconMenu"),buttonMovieSooon=document.getElementById("em-breve"),buttonMoviesReleased=document.getElementById("ja-lacados"),filmesContainer=document.querySelector(".show__filmes__container"),thumbnail=document.querySelector(".thumbnail"),showFilmes=document.querySelector("#movies"),sinopse=document.querySelector("#sinopse"),elenco=document.querySelector("#elenco--vozes"),dublagem=document.querySelector("#dublagem_brasileira"),recepcao=document.querySelector("#recepcao"),trilhaSonora=document.querySelector("#trilha_sonora");let showFilmesRound=Math.round(showFilmes.getBoundingClientRect().y),sinopseRound=Math.round(sinopse.getBoundingClientRect().y),elencoRound=Math.round(elenco.getBoundingClientRect().y),dublagemRound=Math.round(dublagem.getBoundingClientRect().y),recepcaoRound=Math.round(recepcao.getBoundingClientRect().y),trilhaSonoraRound=Math.round(trilhaSonora.getBoundingClientRect().y);console.log(showFilmesRound),console.log(sinopseRound),console.log(elencoRound),console.log(dublagemRound),console.log(recepcaoRound),console.log(trilhaSonoraRound);const contentMovieReleased=`<div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/transformes-1.jpg" alt=" transformes-1">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/transformers-2-revenge-of-the-falen.jpg" alt="transformers 2 revenge of the falen">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/Transformers-O-Lado-culto-da-Lua.jpg" alt="Transformers O Lado culto da Lua">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/Transformers-Age-of-Extinction.jpg" alt="Transformers Age of Extinction">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/Transformers-The-Last-Knight.jpg" alt="Transformers The Last Knight">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/bumvlebee.jpg" alt="bumvlebee">
+                            </div>`,contentMovieSooon=`<div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/transformers8_2.jpg" alt="Transformers: O Despertar das Feras">
+                            </div>`;let clickMenu=!1;function clickButtonHeader(e){isActive(e)}function isActive(e){var o=document.querySelectorAll("[data-link-button-a]");for(let e=0;e<o.length;e++)o[e].classList.remove("header__nav__a--is--active");null!=e&&(o[e].classList.add("header__nav__a--is--active"),3!=e&&2!=e||o[3==e?2:3].classList.add("header__nav__a--is--active"))}function menu(){clickMenu=0==clickMenu?(navMobile.classList.remove("header__nav__mobile--is--closed"),navMobile.classList.add("header__nav__mobile--is--open"),iconMenu.setAttribute("src","./dist/shared/images/icon/menux.svg"),!0):(navMobile.classList.remove("header__nav__mobile--is--open"),navMobile.classList.add("header__nav__mobile--is--closed"),iconMenu.setAttribute("src","./dist/shared/images/icon/menu.svg"),!1)}function buttonSectionShowFilmes(e){"em-breve"==e?(buttonMovieSooon.classList.add("button--is--active"),buttonMoviesReleased.classList.remove("button--is--active"),filmesContainer.innerHTML=contentMovieSooon):(buttonMoviesReleased.classList.add("button--is--active"),buttonMovieSooon.classList.remove("button--is--active"),filmesContainer.innerHTML=contentMovieReleased)}document.addEventListener("scroll",function(){console.log(window.scrollY),0<=window.scrollY&&window.scrollY<showFilmesRound?isActive(null):showFilmesRound<=window.scrollY&&window.scrollY<sinopseRound?isActive(0):sinopseRound<=window.scrollY&&window.scrollY<elencoRound?isActive(1):elencoRound<=window.scrollY&&window.scrollY<dublagemRound?isActive(2):recepcaoRound<=window.scrollY&&window.scrollY<trilhaSonoraRound?isActive(4):trilhaSonoraRound<=window.scrollY&&window.scrollY<2e3&&isActive(5)});
--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -1,19 +1,136 @@
+//elements
 const navMobile = document.querySelector('#nav-mobile');
 const iconMenu = document.querySelector('#iconMenu');
+const buttonMovieSooon = document.getElementById("em-breve");
+const buttonMoviesReleased = document.getElementById("ja-lacados");
+const filmesContainer= document.querySelector('.show__filmes__container');
+const thumbnail = document.querySelector('.thumbnail');
+const showFilmes = document.querySelector('#movies');
+const sinopse = document.querySelector('#sinopse');
+const elenco = document.querySelector('#elenco--vozes');
+const dublagem = document.querySelector('#dublagem_brasileira');
+const recepcao = document.querySelector('#recepcao');
+const trilhaSonora = document.querySelector('#trilha_sonora');
+
+
+let showFilmesRound = Math.round(showFilmes.getBoundingClientRect().y);
+let sinopseRound = Math.round(sinopse.getBoundingClientRect().y);
+let elencoRound = Math.round(elenco.getBoundingClientRect().y);
+let dublagemRound = Math.round(dublagem.getBoundingClientRect().y);
+let recepcaoRound = Math.round(recepcao.getBoundingClientRect().y);
+let trilhaSonoraRound = Math.round(trilhaSonora.getBoundingClientRect().y);
+
+console.log(showFilmesRound);
+console.log(sinopseRound);
+console.log(elencoRound);
+console.log(dublagemRound);
+console.log(recepcaoRound);
+console.log(trilhaSonoraRound);
+
+//consts of the program
+const contentMovieReleased = `<div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/transformes-1.jpg" alt=" transformes-1">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/transformers-2-revenge-of-the-falen.jpg" alt="transformers 2 revenge of the falen">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/Transformers-O-Lado-culto-da-Lua.jpg" alt="Transformers O Lado culto da Lua">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/Transformers-Age-of-Extinction.jpg" alt="Transformers Age of Extinction">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/Transformers-The-Last-Knight.jpg" alt="Transformers The Last Knight">
+                            </div>
+
+                            <div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/bumvlebee.jpg" alt="bumvlebee">
+                            </div>`;
+
+const contentMovieSooon = `<div class="show__filmes__container__item">
+                                <img src="./dist/shared/images/transformers8_2.jpg" alt="Transformers: O Despertar das Feras">
+                            </div>`;
+
 let clickMenu  = false;
+
+
+function clickButtonHeader(index){
+    isActive(index);
+}
+
+document.addEventListener('scroll', function(){
+console.log(window.scrollY)
+
+
+    if(0 <= window.scrollY && window.scrollY < showFilmesRound){
+        isActive(null);
+    }
+    else if(showFilmesRound <= window.scrollY && window.scrollY < sinopseRound){
+        isActive(0);
+    }
+    else if( sinopseRound <= window.scrollY && window.scrollY < elencoRound){
+        isActive(1);
+    }
+    else if(elencoRound <=  window.scrollY && window.scrollY < dublagemRound){
+        isActive(2);
+    }
+    else if(recepcaoRound <= window.scrollY && window.scrollY < trilhaSonoraRound){
+        isActive(4);
+    }
+    else if(trilhaSonoraRound <= window.scrollY && window.scrollY < 2000){
+        isActive(5);
+    }
+    
+})
+
+function isActive(index){
+    const buttons = document.querySelectorAll('[data-link-button-a]');
+
+    for(let i = 0; i < buttons.length; i ++){
+        buttons[i].classList.remove("header__nav__a--is--active");
+    }
+
+    if(index != null){
+
+        buttons[index].classList.add('header__nav__a--is--active');
+    
+        if(index == 3 || index == 2){
+            buttons[index == 3? 2: 3].classList.add('header__nav__a--is--active')
+        }   
+    }
+}
+
 
 
 function menu(){
     if(clickMenu == false){
         navMobile.classList.remove('header__nav__mobile--is--closed');
         navMobile.classList.add('header__nav__mobile--is--open');
-        iconMenu.setAttribute('src', './dist/shared/image/icon/menux.svg');
+        iconMenu.setAttribute('src', './dist/shared/images/icon/menux.svg');
         clickMenu = true;
     }
     else{
         navMobile.classList.remove('header__nav__mobile--is--open');
         navMobile.classList.add('header__nav__mobile--is--closed');
-        iconMenu.setAttribute('src', './dist/shared/image/icon/menu.svg');
+        iconMenu.setAttribute('src', './dist/shared/images/icon/menu.svg');
         clickMenu = false
+    }
+}
+
+function buttonSectionShowFilmes(identifier){
+    if(identifier == "em-breve"){
+        buttonMovieSooon.classList.add('button--is--active');
+        buttonMoviesReleased.classList.remove('button--is--active');
+        filmesContainer.innerHTML = contentMovieSooon;    
+    }
+    else{
+        buttonMoviesReleased.classList.add('button--is--active');
+        buttonMovieSooon.classList.remove('button--is--active'); 
+        filmesContainer.innerHTML = contentMovieReleased;   
     }
 }
